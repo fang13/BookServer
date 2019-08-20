fcce26fe80e08c36ca362adbdde9282c64dff36f
zhouhua <zhouhua25@qq.com>
2019-08-20 22:55:39 +0800

    解决跨域报错问题

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,17 @@ app.use((req, res, next) => {
   Util.timeLog();
   next(); 
 })
+app.all("*",function (req,res,next) {
+    
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1');
+  res.header("Content-Type", "application/json;charset=utf-8");
+
+  next();
+});
+
     
 app.get('/', function (req, res) {
   res.send('Home Page');
